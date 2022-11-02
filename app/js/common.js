4be2a7c0b8d89eb945e4b07bc47f4b0f4f43dcc9
adilettoksobaev <a.toksobaev@nambaone.app>
2022-11-02 18:54:25 +0600

    loader

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,11 @@
 $(function () {
+  setTimeout(function () {
+    var preloader = document.getElementById("page-preloader");
+    if (!preloader.classList.contains("done")) {
+      preloader.classList.add("done");
+    }
+  }, 1500);
+
   $(".main-carousel").slick({
     dots: false,
     infinite: false,
